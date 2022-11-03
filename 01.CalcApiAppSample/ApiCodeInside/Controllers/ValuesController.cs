dea005822e8caff75b112cfc2f517302d2a188ff
A.Halim Zaki <a.halimzaki@gmail.com>
2022-11-03 18:26:57 +0200

    first-commit

---
+++ 01.CalcApiAppSample/ApiCodeInside/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using CalcClass;
+
+namespace ApiCodeInside.Controllers
+{
+    public class ValuesController : Controller
+    {
+        // GET: api/<ValuesController>
+
+         
+
+        Calc calc = new Calc();
+
+
+
+        [HttpGet("/average")]
+
+        public int Average([FromQuery] int[] nums)
+
+        {
+
+            return calc.Average(nums);
+
+        }
+
+        [HttpGet("/smallest")]
+
+        public int Smallest([FromQuery] int[] nums)
+
+        {
+
+            return calc.Smallest(nums);
+
+        }
+
+        [HttpGet("/largest")]
+
+        public int Largest([FromQuery] int[] nums)
+
+        {
+
+            return calc.Largest(nums);
+
+        }
+
+    }
+
+}
+
